e634af89c907d71cee53f317961b74fd42b001e2
Michael Mueller <michael.mueller@uos.de>
2023-03-30 15:50:43 +0200

    Added stub for future implementation of Intel performance counters.

---
+++ include/intel_hpc.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file intel_hpc.hpp
+ * @author Michael Müller (michael.mueller@uos.de)
+ * @brief Intel Performance Counter Interface
+ * @version 0.1
+ * @date 2022-12-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+#include "msr.hpp"
+
+class Intel_hpc
+{
+};
