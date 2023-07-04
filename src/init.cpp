af3a087934e88f43ec2b8547d7195cf29b319cfa
Christian Helmuth <christian.helmuth@genode-labs.com>
2023-07-04 13:30:49 +0200

    Remove time/date from banner for reproducible builds
    Fixes #10

--- src/init.cpp
+++ src/init.cpp
@@ -91,7 +91,7 @@ void init (mword magic, mword mbi)
     for (void (**func)() = &CTORS_C; func != &CTORS_G; (*func++)()) ;
 
     // Now we're ready to talk to the world
-    Console::print ("\fNOVA Microhypervisor v%d-%07lx (%s): %s %s [%s] [%s]\n", CFG_VER, reinterpret_cast<mword>(&GIT_VER), ARCH, __DATE__, __TIME__, COMPILER_STRING, magic == Multiboot::MAGIC ? "MBI" : (magic==Multiboot2::MAGIC ? "MBI2" : ""));
+    Console::print ("\fNOVA Microhypervisor v%d-%07lx (%s): [%s] [%s]\n", CFG_VER, reinterpret_cast<mword>(&GIT_VER), ARCH, COMPILER_STRING, magic == Multiboot::MAGIC ? "MBI" : (magic==Multiboot2::MAGIC ? "MBI2" : ""));
 
     Idt::build();
     Gsi::setup();
