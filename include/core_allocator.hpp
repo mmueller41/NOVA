4cba7d8b292c35e42d52f55251b0f3f24a5c8511
Michael Mueller <michael.mueller@uos.de>
2024-02-06 18:31:22 +0100

    Rewrote core allocation algorithm and automatically activate the cell's worker on a core returned to it.

--- include/core_allocator.hpp
+++ include/core_allocator.hpp
@@ -15,10 +15,13 @@
 class Core_allocator
 {
     private:
+        /* Bit allocator and free map for CPU cores */
         Bit_alloc<NUM_CPU, 0> free_map{};
+        /* Bitmap saving which cores are idle */
         mword idle_mask[NUM_CPU / (sizeof(mword) * 8)];
         Cell *owners[NUM_CPU]; /* saves which cell owns a certain core */
-        Cell *borrowers[NUM_CPU];
+        /* borrowers saves which cell has borrowed which CPU core */
+        volatile Cell *borrowers[NUM_CPU];
         Spinlock dump_lock{};
 
         ALWAYS_INLINE
@@ -45,10 +48,14 @@ class Core_allocator
                 long cpu = 0;
                 while ((cpu = bit_scan_forward((yield_mask))) != -1) {
                     yield_mask &= ~(1UL << cpu);
+                    //trace(0, "Will try to reclaim core %lu for %p from %p", cpu, claimant, borrowers[cpu]);
                     if (borrowers[cpu] && borrowers[cpu] != claimant)
                     {
-                        //trace(0, "Reclaiming core %lu for %p", cpu, claimant);
-                        reclaimed += borrowers[cpu]->yield_cores(1UL<<cpu);
+                        if (cpu == Cpu::id)
+                            continue;
+                        //trace(0, "Reclaiming core %lu for %p from %p", cpu, claimant, borrowers[cpu]);
+                        Cell *borrower = const_cast<Cell*>(__atomic_load_n(&borrowers[cpu], __ATOMIC_SEQ_CST));
+                        reclaimed += borrower->yield_cores(1UL << cpu);
                     }
                 }
             }
@@ -71,55 +78,39 @@ class Core_allocator
                 /* First we try to allocate a core from the pre-reserved cores of clamaint, only
                 if the claiming cell has exhaused its reserved cores, we try to allocate a core from
                 the global core allocator. This way the cores initially assigned to the cell by Hoitaja are used first, before allocating cores from other cells. */
-                /*Spinlock log_lock;
-                log_lock.lock();*/
-                //trace(0, "Core Allocation with mask: %lx", *mask);
-                //free_map.dump_trace();
-                /*log_lock.unlock();*/
 
-                while (trials < 3) {
-                    /* First try to allocate a core from claimant's reserve pool */
-                    cpu_id = static_cast<unsigned int>(free_map.alloc_with_mask(mask));
-                    if (!cpu_id) {
-                        /* If this fails, we try to get an idle core */
-                        cpu_id = static_cast<unsigned int>(free_map.alloc_with_mask(idle_mask));
-                    } else
-                        break;
-                    if (!cpu_id)
-                    {
-                        /* If there are no idle cores, check wether we can reclaim cores that claimant borrowed to another cell */
-                        while (reclaim_cores(claimant, cores)) {
-                        //trace(0, "Reclaimed %d cores for %p", reclaimed, claimant);
-                            cpu_id = static_cast<unsigned int>(free_map.alloc_with_mask(mask));
-                            if (cpu_id) {
-                                //trace(0, "%p: Retook CPU %u", claimant, cpu_id);
-                                break;
-                            }
-                        }
-                    } else
-                        break;
-                    if (cpu_id)
-                        break;
-                    trials++;
-                }
+                /* First try to allocate a core from claimant's reserve pool */
+                cpu_id = static_cast<unsigned int>(free_map.alloc_with_mask(mask));
 
                 if (!cpu_id) {
-                    /* If we could still not find a core, all cores must have been consumed. Hence, we let the allocation fail by returning an allocation of zero cores. */
+                    /* If this fails, we try to get an idle core for three times */
+                    for (; !cpu_id && trials < 3; trials++) 
+                        cpu_id = static_cast<unsigned int>(free_map.alloc_with_mask(idle_mask));
+                } 
+
+                if (!cpu_id)
+                {
+                    /* If there are no idle cores, check wether we can reclaim cores that claimant borrowed to another cell.
+                    This is performed asynchronously to avoid a deadlock, if the borrower it self claims cores from the claimant. However, we return the cores we could already allocate, if any. */
+                    reclaim_cores(claimant, cores);
                     return core_allocation;
                 }
 
+                /* If we allocated an idling core, we have to check wether it is one of the claimant's own cores or if it is owned by another cell. For the latter case, the claimant has to be recorded as a borrower of this core. This is needed so that the owner can reclaim the core, if necessary. */
                 Atomic::test_set_bit<mword>(core_allocation, cpu_id);
                 borrowed = !is_owner(claimant, cpu_id);
 
                 if (borrowed) {
                     claimant->borrowed_cores |= (1UL << cpu_id);
                     __atomic_store_n(&borrowers[cpu_id], claimant, __ATOMIC_SEQ_CST);
-                } else {
+                }
+                else
+                {
                     owners[cpu_id] = claimant;
                 }
+
+                /* Mark the core as busy now, by clearing the corresponding bit in the idle cores bitmap */
                 Atomic::test_clr_bit<mword>(idle_mask[cpu_id / NUM_CPU], cpu_id % NUM_CPU);
-                 //trace(0, "Allocated core %d: allocation=%lx, in_mask : %d", cpu_id, core_allocation, is_owner(claimant, cpu_id));
-                // free_map.dump_trace();
             }
             return core_allocation;
         }
@@ -127,8 +118,6 @@ class Core_allocator
         void yield(unsigned int cpu_id) {
             Atomic::test_set_bit<mword>(idle_mask[cpu_id / NUM_CPU], cpu_id % NUM_CPU);
             free_map.release(cpu_id);
-            if (borrowers[cpu_id])
-                __atomic_store_n(&borrowers[cpu_id], 0, __ATOMIC_SEQ_CST);
         }
 
         void dump_allocation() {
@@ -137,10 +126,18 @@ class Core_allocator
 
         bool reserve(Cell *reservant, mword const id) {
             /* It is possible that reservant is a newly created cell or just woke up and its core at <id> has been borrowed by another cell. If that's the case, we reclaim the core <id> for <reservant> by commanding the borrower to yield the core. */
-            if (owners[id] == reservant && borrowers[id]) {
-                borrowers[id]->yield_cores((1UL << id));
+            if (owners[id] == reservant && __atomic_load_n(&borrowers[id], __ATOMIC_SEQ_CST)) {
+                Cell *borrower = const_cast<Cell *>(__atomic_load_n(&borrowers[id], __ATOMIC_SEQ_CST));
+                //trace(0, "%p: Need to reclaim CPU %ld from %p", reservant, id, borrower);
+                borrower->yield_cores((1UL << id));
+            } else if (owners[id] != reservant) {
+                return false;
             }
-            return free_map.reserve(id);
+            //trace(0, "Reserving CPU %ld for cell %p", id, reservant);
+            free_map.reserve(id);
+            reservant->core_map |= (1UL << id);
+                return true;
+            
         }
 
         ALWAYS_INLINE
@@ -177,8 +174,14 @@ class Core_allocator
         void return_core(Cell *borrower, unsigned cpu) {
             if (borrowers[cpu] == borrower)
                 __atomic_store_n(&borrowers[cpu], 0, __ATOMIC_SEQ_CST);
+            Atomic::test_set_bit<mword>(owners[cpu]->core_map, cpu);
+            owners[cpu]->wake_core(cpu);
         }
-        
+
+        bool borrowed(Cell *cell, unsigned cpu) {
+            return borrowers[cpu] == cell;
+        }
+
         inline void dump_cells() 
         {
             Lock_guard<Spinlock> guard(dump_lock);
